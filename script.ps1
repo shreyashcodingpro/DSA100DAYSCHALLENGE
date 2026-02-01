e512cca296353d9a6c2d193a0232c64432ef9eac
Your Name <shreyashagarwal36@gmail.com>
2026-02-02 00:00:00 +0530

    Test commit

--- script.ps1
+++ script.ps1
@@ -1,10 +1,8 @@
-for ($i=0; $i -lt 90; $i++) {
-    $date = (Get-Date "2026-02-02").AddDays($i)
+$date = Get-Date "2026-02-02"
 
-    $env:GIT_AUTHOR_DATE = $date.ToString("yyyy-MM-ddTHH:mm:ss")
-    $env:GIT_COMMITTER_DATE = $env:GIT_AUTHOR_DATE
+$env:GIT_AUTHOR_DATE = $date.ToString("yyyy-MM-ddTHH:mm:ss")
+$env:GIT_COMMITTER_DATE = $env:GIT_AUTHOR_DATE
 
-    "Day $($i+1)" | Out-File -Append log.txt
-    git add .
-    git commit -m "Day $($i+1) commit"
-}
+"Test commit" | Out-File -Append log.txt
+git add .
+git commit -m "Test commit"
