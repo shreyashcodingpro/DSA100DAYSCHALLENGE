ad0c248f9ed5619846993fe52b7e562ee23f9248
Your Name <shreyashagarwal36@gmail.com>
2026-02-03 00:00:00 +0530

    Day 1 commit

--- script2.ps1
+++ script2.ps1
@@ -1,10 +1,12 @@
 for ($i=1; $i -le 100; $i++) {
+
     $date = (Get-Date "2026-02-02").AddDays($i)
 
     $env:GIT_AUTHOR_DATE = $date.ToString("yyyy-MM-ddTHH:mm:ss")
     $env:GIT_COMMITTER_DATE = $env:GIT_AUTHOR_DATE
 
-"Day $i" | Out-File log.txt -Append
+    Add-Content log.txt "Day $i $(Get-Date)"
+
     git add .
     git commit -m "Day $i commit"
 }
